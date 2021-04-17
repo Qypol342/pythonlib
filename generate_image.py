f187bdd70bcad3e0d803e835fdab29d687f2dab9
Maeght Loan <37497007+Qypol342@users.noreply.github.com>
2021-04-17 18:16:23 +0200

    Update generate_image.py

--- generate_image.py
+++ generate_image.py
@@ -4,6 +4,7 @@ from pythonlib.findface import search_face
 import shutil
 import time
 from pythonlib.nn import calculate
+from PIL import Image
 
 async def f():
 	picture = await get_online_person()  # bytes representation of the image
@@ -18,18 +19,18 @@ def gen_im():
 
 	search_face("abp.png")
 	#shutil.copy('images/_faces.png','tmp/'+'intrus'+'_'+str(time.time())+'.png')
-
-for i in range(100):
+def run_gen_im():
 	r = 0
 	count = 0
 	while r == 0:	
 		gen_im()
-		from PIL import Image
+		
 		s = Image.open('images/_faces.png')
 		r = calculate(s)
 		conv =['intrus','loan']
-		#print(conv[r])
+
 		count +=1
 	#s.show()
 	shutil.copy('images/_faces.png','tmp/'+'intrus'+'_'+str(time.time())+'.png')
-	print(1/count)
+
+
